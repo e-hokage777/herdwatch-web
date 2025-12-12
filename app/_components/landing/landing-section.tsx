cecff3fff1b5602a63fd10c4e26c32ea9bb8c62e
e-hokge777 <e.hokage777@gmail.com>
2025-12-12 23:01:58 +0000

    deleted unused component

--- app/_components/landing/landing-section.tsx
+++
@@ -1,9 +0,0 @@
-export default function LandingSection({
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <section className="h-dvh w-full flex flex-row justify-between items-center py-8 bg-gray-100">
-      <div className="container">{props.children}</div>
-    </section>
-  );
-}
